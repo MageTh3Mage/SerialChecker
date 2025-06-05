a87737423c14324c28326290860e89e6e9eb09d0
Mage <connaroegarrett@gmail.com>
2025-06-04 22:42:06 -0500

    Update SerialChecker.bat

--- SerialChecker.bat
+++ SerialChecker.bat
@@ -1,5 +1,4 @@
 @echo off
-::chcp 65001 >nul
 title Mage's Serial Checker
 
 for /F %%A in ('echo prompt $E ^| cmd') do set "ESC=%%A"
@@ -16,12 +15,12 @@ cls
 echo %PURPLE%Grabbing Serials, please wait.%RESET%
 echo.
 
-:: Disk Serial Number
+:: Disk
 for /f "delims=" %%A in ('powershell -command "Get-PhysicalDisk | ForEach-Object { $_.SerialNumber }"') do (
     set "Disk=%%A"
 )
 
-:: Motherboard Serial
+:: Motherboard
 for /f "delims=" %%A in ('powershell -command "(Get-CimInstance Win32_BaseBoard).SerialNumber"') do (
     set "Motherboard=%%A"
 )
@@ -36,12 +35,12 @@ for /f "delims=" %%A in ('powershell -command "(Get-CimInstance Win32_BIOS).SMBI
     set "BIOS=%%A"
 )
 
-:: MAC Address (first active adapter)
+:: MAC Address
 for /f "delims=" %%A in ('powershell -command "(Get-NetAdapter | Where-Object { $_.Status -eq 'Up' })[0].MacAddress"') do (
     set "MAC=%%A"
 )
 
-:: Volume Serial Number (VolumeID) for C: drive
+:: VolumeID (C: Drive)
 for /f "tokens=5 delims= " %%A in ('vol C: ^| find "Serial"') do (
     set "VolumeID=%%A"
 )
@@ -56,15 +55,15 @@ if not defined VolumeID set "VolumeID=N/A"
 cls
 echo.
 echo %PURPLE%                         %LIGHTPURPLE%System Serials%PURPLE%%RESET%
-echo %PURPLE%______________________________________________________________%RESET%
+echo %PURPLE%______________________________________________________________  %RESET%
 echo.
 echo %PURPLE%   Disk:          %LIGHTPURPLE%!Disk!%PURPLE%                   %RESET%
 echo %PURPLE%   Motherboard:   %LIGHTPURPLE%!Motherboard!%PURPLE%            %RESET%
 echo %PURPLE%   UUID:          %LIGHTPURPLE%!UUID!%PURPLE%                   %RESET%
 echo %PURPLE%   BIOS:          %LIGHTPURPLE%!BIOS!%PURPLE%                   %RESET%
 echo %PURPLE%   MAC:           %LIGHTPURPLE%!MAC!%PURPLE%                    %RESET%
-echo %PURPLE%   VolumeID (C:): %LIGHTPURPLE%!VolumeID!%PURPLE%            %RESET%
-echo %PURPLE%______________________________________________________________%RESET%
+echo %PURPLE%   VolumeID (C:): %LIGHTPURPLE%!VolumeID!%PURPLE%               %RESET%
+echo %PURPLE%______________________________________________________________  %RESET%
 echo.
 echo.
 pause
